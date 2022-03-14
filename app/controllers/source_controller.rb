53b2ef98edb8cee18421620fced8899a5ef8dc4d
nbellew <nbellew@uccs.edu>
2022-03-14 02:01:52 +0000

    Added some rspec tests

--- app/controllers/source_controller.rb
+++ app/controllers/source_controller.rb
@@ -1,4 +1,14 @@
 class SourceController < ApplicationController
   def index
   end
+
+  #method for taking url from search bar
+  def get_url 
+    return "https://www.wikipedia.org/"
+  end
+
+  #method for getting array of sources from url
+  def get_sources
+    return ["url1", "url2", "url3"]
+  end
 end
