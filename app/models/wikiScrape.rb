3eee3ec48c79ac8156b1a018f3a70be2af9f7b09
root <rileystaite@gmail.com>
2022-04-05 12:41:23 +0000

    Removed mw style glitch from parsed code

--- app/models/wikiScrape.rb
+++ app/models/wikiScrape.rb
@@ -7,10 +7,11 @@ class WikipediaScraper
         # Get this link from searchbar
         wiki_link = "https://en.wikipedia.org/wiki/Ruby_(programming_language)"
         file = URI.open(wiki_link)
-        page_data = Nokogiri::HTML5(file)    
-        reflist = page_data.css('.reference-text')
+        page_data = Nokogiri::HTML5(file)
+        page_data.css('style').remove  
 
-        reflist.each do |reference|
+        reflist = page_data.css('.reference-text')
+        reflist.each do |reference| 
             url = reference.css('a').attribute('href').value
             content = reference.text
             p content
@@ -22,15 +23,21 @@ end
 scrape = WikipediaScraper.new
 scrape.scrape_references  
 
-    #For improvement:
+    # Improvements:
+    # For sprint 2-2:
+    # 1. Remove \ and "" characters
+    # 2. Place in hash 
+    # 3. Implement in views + controller
+    # 4. Get wiki link from stream
+
+    # After sprint 2-2
     # 1. Possibly remove "retreived on" dates, also takes the form of (xyz, 20xx)
     # 2. Add "..." and cut off long reference descriptions
     # 3. If reference links contain aria-label="Jump Up" inside reflist url, 
     #   Then remove div references but only the first one
     #   That is the "notes" section which is a reflist class on wiki pages  
 
-
-    #Old testing code, will be gone
+    # Old testing code, will be gone
         #reflist_links = page_data.css(".references").css("a")
         #p page_data.xpath('//div[@class="reference-text"]/a').map { |link| link['href'] }
         #reference_list = []
