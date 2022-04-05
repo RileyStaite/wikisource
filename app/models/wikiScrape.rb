c85b98a1339eda7856877c73012ba4322d33f3b0
root <rileystaite@gmail.com>
2022-04-05 12:02:20 +0000

    cleaned up wikiScrape

--- app/models/wikiScrape.rb
+++ app/models/wikiScrape.rb
@@ -4,41 +4,33 @@ require 'open-uri'
 class WikipediaScraper
 
     def scrape_references
-    #Get this link from searchbar
+        # Get this link from searchbar
         wiki_link = "https://en.wikipedia.org/wiki/Ruby_(programming_language)"
         file = URI.open(wiki_link)
         page_data = Nokogiri::HTML5(file)    
-        reflist_links = page_data.css('.reference-text')
-        reflist_content = page_data.css('.reference-text')
+        reflist = page_data.css('.reference-text')
 
-        puts reflist_content
-
-        reflist_links.each do |reference|
+        reflist.each do |reference|
             url = reference.css('a').attribute('href').value
             content = reference.text
             p content
             p url
         end
-        #reflist_links = page_data.css(".references").css("a")
-        #p page_data.xpath('//div[@class="reference-text"]/a').map { |link| link['href'] }
-        #reference_list = []
-        #Testing
-        #p page_data
-        #p reflist
-        #p reflist_links
-        #p file
     end
-
 end
 
 scrape = WikipediaScraper.new
 scrape.scrape_references  
 
     #For improvement:
-    #Remove all text ending in .mw-parser-output
-    #Remove retreived, also takes the form of (xyz, 20xx)
-    #Add "..." and cut off long reference descriptions
-    #If page_data contains aria-label="Jump Up" inside a references list, 
-    #Then remove div references but only the first one
-    # ^ That is the "notes" section which is a reflist class on wiki pages  
+    # 1. Possibly remove "retreived on" dates, also takes the form of (xyz, 20xx)
+    # 2. Add "..." and cut off long reference descriptions
+    # 3. If reference links contain aria-label="Jump Up" inside reflist url, 
+    #   Then remove div references but only the first one
+    #   That is the "notes" section which is a reflist class on wiki pages  
 
+
+    #Old testing code, will be gone
+        #reflist_links = page_data.css(".references").css("a")
+        #p page_data.xpath('//div[@class="reference-text"]/a').map { |link| link['href'] }
+        #reference_list = []
