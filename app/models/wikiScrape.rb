e9a2d22795c383b00b7395388a56065bc5b45072
root <rileystaite@gmail.com>
2022-04-06 00:20:37 +0000

    content array, url array, source_hash (combined)

--- app/models/wikiScrape.rb
+++ app/models/wikiScrape.rb
@@ -4,26 +4,33 @@ require 'open-uri'
 class WikipediaScraper
 
     def scrape_references
-        # Get this link from searchbar
-        wiki_link = "https://en.wikipedia.org/wiki/Ruby_(programming_language)"
+        wiki_link = "https://en.wikipedia.org/wiki/Ruby_(programming_language)" # Get this link from searchbar
         file = URI.open(wiki_link)
         page_data = Nokogiri::HTML5(file)
-        page_data.css('style').remove  
+        page_data.css('style').remove # Removes a style glitch inside reference classes on some wiki pages by mediawiki api
+        reflist = page_data.css('.reference-text') # Only grabs data from class 'reference-text'
+        
+        content_array = [] # holds reference info
+        url_array = [] # holds reference link
+        source_hash = [] # info is key, link is data
+        i = 0
 
-        sources = []
-        reflist = page_data.css('.reference-text')
         reflist.each do |reference| 
             content = reference.text
             url = reference.css('a').attribute('href').value
+            content.tr!('"', '') # remove backslashes
+            url.tr!('"', '') # remove backslashes
 
-            # these remove all the backslashes
-            url.tr!('"', '')
-            content.tr!('"', '')
-            # test print
-            puts content
-            puts url
+            content_array[i] = content
+            url_array[i] = url
+            # puts content_array[i] # testing
+            # puts url_array[i] # testing
+            i += 1
+            # puts content # testing
+            # puts url # testing
         end
-        # return sources
+        source_hash = Hash[content_array.zip(url_array)]
+        puts source_hash # testing
     end
 end
 
@@ -32,10 +39,8 @@ scrape.scrape_references
 
     # Improvements:
     # For sprint 2-2:
-    # 1. Remove \ and "" characters
-    # 2. Place in hash 
-    # 3. Implement in views + controller
-    # 4. Get wiki link from stream
+    # 1. Implement in views + controller
+    # 2. Get wiki link from stream
 
     # After sprint 2-2
     # 1. Possibly remove "retreived on" dates, also takes the form of (xyz, 20xx) (or store these for sorting)
@@ -43,8 +48,3 @@ scrape.scrape_references
     # 3. If reference links contain aria-label="Jump Up" inside reflist url, 
     #   Then remove div references but only the first one
     #   That is the "notes" section which is a reflist class on wiki pages  
-
-    # Old testing code, will be gone
-        #reflist_links = page_data.css(".references").css("a")
-        #p page_data.xpath('//div[@class="reference-text"]/a').map { |link| link['href'] }
-        #reference_list = []
