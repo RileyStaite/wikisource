1ae86e693a4f9260ddc263ac0543eaaa361c4b90
nbellew <nbellew@uccs.edu>
2022-04-09 19:43:54 +0000

    Background gradient dynically changes after getting sources

--- spec/controllers/source_controller_spec.rb
+++ spec/controllers/source_controller_spec.rb
@@ -2,25 +2,14 @@ require 'rails_helper'
 
 RSpec.describe SourceController, type: :controller do
 
+
+############### Add new tests here ###############
+
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+
   end
 
-  describe "Expect #get_url" do
-    it "return a string" do
-      url = controller.get_url
-      expect(url).to be_a_kind_of String
-    end
-  end
-
-  describe "Expect #get_sources" do
-    it "returns an array of strings" do
-      expect(controller.get_sources).to be_an_instance_of(Array)
-    end
-  end
+##################################################
 
 
 end
