283044b5267699c69171a48b79567a2b711d6b5a
nbradhamUCCS <nbradham@uccs.edu>
2022-04-15 17:14:34 +0000

    fixed initial tests.

--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Reference, type: :model do
     it "is not valid without a url" do
-        sources = Reference.parse(nil)
-        expect(sources).to raise_error
+        expect{Reference.parse(nil)}.to raise_error
     end
   it "is valid with a url" do
     link = "https://en.wikipedia.org/wiki/University_of_Colorado_Colorado_Springs"
@@ -13,7 +12,7 @@ RSpec.describe Reference, type: :model do
   it "should return a hash if valid" do
     link = "https://en.wikipedia.org/wiki/University_of_Colorado_Colorado_Springs"
     sources = Reference.parse(link)
-    expect(sources).to be_a(hash)
+    expect(sources).to be_a(Hash)
   end
 
 end
